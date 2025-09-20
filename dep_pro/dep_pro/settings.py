54173e9e2e2a6191b0d6bf3bd9202ca3682e3790
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-09-20 12:09:25 +0530

    added req file

--- dep_pro/dep_pro/settings.py
+++ dep_pro/dep_pro/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ 
+env=environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +79,11 @@ WSGI_APPLICATION = 'dep_pro.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "django_sample",
-        "USER":"root","PASSWORD":"root","HOST":"localhost","PORT":"3306"
+        'NAME': env("Database name"),
+        "USER":env("User"),
+        "PASSWORD":env("Password"),
+        "HOST":env("Host"),
+        "PORT":env("Port")
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
